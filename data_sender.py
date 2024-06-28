47f7068c995a59f810566003ac74b89d2434ceed
luca <lucafalasca08@gmail.com>
2024-06-28 19:23:19 +0200

    timer

--- data_sender.py
+++ data_sender.py
@@ -1,12 +1,15 @@
 import requests
 import csv
 import time
+from datetime import datetime
 
 url = 'http://localhost:5200/listener'
 file_path = './data/dataset/raw_data_medium-utv_sorted.csv'
 headers = {
             'Content-Type': 'text/csv'
         }
+
+formato = "%Y-%m-%dT%H:%M:%S.%f"
 
 def upload_file(url, file_path):
     # Open the file in binary mode and send it in the POST request
@@ -20,16 +23,22 @@ def upload_file(url, file_path):
 def read_file_and_send_data(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
-        b = False
+        next(reader)
+        last_time = None
         for row in reader:
-            if not b:
-                b = True
-                continue
+            if last_time is None:
+                last_time = datetime.strptime(row[0], formato)
+            else:
+                new_time = datetime.strptime(row[0], formato)
+                delay = (new_time - last_time) 
+                delay_seconds = delay.total_seconds() / 86400
+                print("T:" + str(delay_seconds))
+                time.sleep(delay_seconds)
+                last_time = new_time
             str_row = str(row)[1:len(str(row)) - 1].replace("'", "")
             print(str_row)
-            response = requests.post(url, data=str_row, headers=headers)
-            print(f'Status Code: {response.status_code}')
-            time.sleep(1)
+            #response = requests.post(url, data=str_row, headers=headers)
+            #print(f'Status Code: {response.status_code}')
 
 if __name__ == "__main__":
     read_file_and_send_data(file_path)
