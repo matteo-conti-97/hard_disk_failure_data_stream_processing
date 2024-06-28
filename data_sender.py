a000c5a06189195476bc27911777a79e6878f396
luca <lucafalasca08@gmail.com>
2024-06-28 18:12:00 +0200

    ingestion nifi + kafka

--- data_sender.py
+++ data_sender.py
@@ -1,8 +1,12 @@
 import requests
 import csv
+import time
 
 url = 'http://localhost:5200/listener'
 file_path = './data/dataset/raw_data_medium-utv_sorted.csv'
+headers = {
+            'Content-Type': 'text/csv'
+        }
 
 def upload_file(url, file_path):
     # Open the file in binary mode and send it in the POST request
@@ -16,10 +20,17 @@ def upload_file(url, file_path):
 def read_file_and_send_data(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
+        b = False
         for row in reader:
-            #row['date']
-            print(row[0])
-
+            if not b:
+                b = True
+                continue
+            str_row = str(row)[1:len(str(row)) - 1].replace("'", "")
+            print(str_row)
+            response = requests.post(url, data=str_row, headers=headers)
+            print(f'Status Code: {response.status_code}')
+            time.sleep(1)
 
 if __name__ == "__main__":
     read_file_and_send_data(file_path)
+    #upload_file(url, file_path)
