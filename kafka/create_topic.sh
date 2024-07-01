857e5108adef11078093e96fb369bcd9e9a19fcf
matteo-conti-97 <matteo.conti.977@gmail.com>
2024-07-01 19:30:09 +0200

    create output topics

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -3,3 +3,12 @@
 /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
 echo "topic $FLINK_TOPIC_NAME was create"
 
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+echo "topic $QUERY1_TOPIC_NAME was create"
+
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+echo "topic $QUERY2_TOPIC_NAME was create"
+
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+echo "topic $QUERY3_TOPIC_NAME was create"
+
