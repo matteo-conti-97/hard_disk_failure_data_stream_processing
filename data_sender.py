4521af497a26376a6bc78fd9c4622cda6200976e
luca <lucafalasca08@gmail.com>
2024-07-01 17:03:49 +0200

    Nifi configuration

--- data_sender.py
+++ data_sender.py
@@ -2,6 +2,8 @@ import requests
 import csv
 import time
 from datetime import datetime
+import sys
+import subprocess
 
 url = 'http://localhost:5200/listener'
 file_path = './data/dataset/raw_data_medium-utv_sorted.csv'
@@ -25,20 +27,30 @@ def read_file_and_send_data(file_path):
         reader = csv.reader(file)
         next(reader)
         last_time = None
+        c = 0
         for row in reader:
             if last_time is None:
                 last_time = datetime.strptime(row[0], formato)
             else:
-                new_time = datetime.strptime(row[0], formato)
+                try:
+                    new_time = datetime.strptime(row[0], formato)
+                except:
+                    continue
                 delay = (new_time - last_time) 
                 delay_seconds = delay.total_seconds() / 86400
                 print("T:" + str(delay_seconds))
-                time.sleep(delay_seconds)
+                #time.sleep(delay_seconds)
+                #time.sleep(0.1)
                 last_time = new_time
             str_row = str(row)[1:len(str(row)) - 1].replace("'", "")
             print(str_row)
-            #response = requests.post(url, data=str_row, headers=headers)
-            #print(f'Status Code: {response.status_code}')
+            print("SENDING DATA!")
+            print("TUPLE: " + str(c))
+            c += 1
+            response = requests.post(url, data=str_row, headers=headers)
+            print("SENDED!")
+            print(f'Status Code: {response.status_code}')
+
 
 if __name__ == "__main__":
     read_file_and_send_data(file_path)
