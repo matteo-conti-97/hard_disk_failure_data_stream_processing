129407fee206bb9ed530086c7cc1708dddcb56aa
matteo-conti-97 <matteo.conti.977@gmail.com>
2024-07-03 18:19:36 +0200

    Ingestion topic kafka creation + latency marker metrics settings in flink

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -12,3 +12,6 @@ echo "topic $QUERY2_TOPIC_NAME was create"
 /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
 echo "topic $QUERY3_TOPIC_NAME was create"
 
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $INGESTION_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+echo "topic $INGESTION_TOPIC_NAME was create"
+
