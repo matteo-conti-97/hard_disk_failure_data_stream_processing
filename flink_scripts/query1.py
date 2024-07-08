2938b457a34abfe37feb09190d9bb478cceeed22
matteo-conti-97 <matteo.conti.977@gmail.com>
2024-07-08 23:22:04 +0200

    Global window

--- flink_scripts/query1.py
+++ flink_scripts/query1.py
@@ -11,13 +11,49 @@ from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.functions import MapFunction, AggregateFunction
 from pyflink.datastream.window import TumblingEventTimeWindows, GlobalWindows
-from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream import StreamExecutionEnvironment,  Trigger, TriggerResult
 from pyflink.common import Time
 from datetime import datetime
+import time
 from math import sqrt
+
 
 format = "%Y-%m-%dT%H:%M:%S.%f"
 
+class MyTrigger(Trigger):
+
+    def __init__(self, idle_time_in_seconds):
+        self.idle_time_in_seconds = idle_time_in_seconds
+        self.last_seen_timestamp = -1
+        self.last_timer_time=-1
+
+    def on_merge(self, window, ctx):
+        return TriggerResult.CONTINUE
+    
+    def on_element(self, element, timestamp, window, ctx):
+        current_time = time.time() * 1000
+        self.last_seen_timestamp = current_time
+        if self.last_timer_time != -1:
+            ctx.delete_processing_time_timer(self.last_timer_time)
+        self.last_timer_time = current_time + 64 *1000
+        ctx.register_processing_time_timer(current_time + 30 * 1000)
+        return TriggerResult.CONTINUE
+
+    def on_processing_time(self, tim, window, ctx):
+        print("TIMER FIRED")
+        if self.last_seen_timestamp == -1:
+            return TriggerResult.CONTINUE
+        current_time = time.time() * 1000
+        if current_time - self.last_seen_timestamp >= self.idle_time_in_seconds * 1000:
+            return TriggerResult.FIRE
+        else:
+            return TriggerResult.CONTINUE
+
+    def on_event_time(self, time, window, ctx):
+        return TriggerResult.CONTINUE
+
+    def clear(self, window, ctx):
+        pass
 
 class ParseCSVFunction(MapFunction):
 
@@ -167,22 +203,25 @@ def query1(win):
     #                 ]))\
     #         .key_by(lambda x: x[1])\
     #         .window(win).reduce(lambda x, y: (x[0], x[1], 1))
-    parsed_stream = (
-        src.map(ParseCSVFunction())
-        .filter(lambda x: x != ("Invalid CSV line"))
+    parsed_stream = src.map(ParseCSVFunction())\
+        .filter(lambda x: x != ("Invalid CSV line"))\
         .assign_timestamps_and_watermarks(
             WatermarkStrategy.for_monotonous_timestamps().with_timestamp_assigner(
                 CustomTimestampAssigner()
             )
-        )
+        )\
         .map(
             lambda x: (x[0], x[4], x[6]),
             output_type=Types.TUPLE([Types.SQL_DATE(), Types.INT(), Types.FLOAT()]),
-        )
+        )\
         .filter(lambda x: 1000 <= x[1] <= 1020)
-        .key_by(lambda x: x[1])
-        .window(win)
-        .aggregate(EventCounter())
+        
+    if isinstance(win, GlobalWindows):
+        parsed_stream=parsed_stream.key_by(lambda x: x[1]).window(win).trigger(MyTrigger(idle_time_in_seconds=25))
+    else:
+        parsed_stream=parsed_stream.key_by(lambda x: x[1]).window(win)
+    
+    parsed_stream=parsed_stream.aggregate(EventCounter())\
         .map(
             lambda x: (x[0], x[1], x[2], x[3], sqrt(x[4])),
             output_type=Types.TUPLE(
@@ -195,10 +234,10 @@ def query1(win):
                 ]
             ),
         )
-    )
+    
     res = parsed_stream.map(lambda x: tuple_to_csv_ser(x), output_type=Types.STRING())
 
-    # parsed_stream.map(PrintFunction())
+    #parsed_stream.map(PrintFunction())
     res.sink_to(sink)
     env.execute()
 
@@ -219,6 +258,7 @@ if __name__ == "__main__":
     elif win_type == str(3):
         print("Window size: Global")
         win = GlobalWindows.create()
+        #win = TumblingEventTimeWindows.of(Time.days(23), Time.days(13))
     else:
         print("Invalid window size exiting...")
         exit()
