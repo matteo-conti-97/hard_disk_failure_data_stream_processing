ee9ab8ac98bc22b758e6376a65129c3d121190aa
matteo-conti-97 <matteo.conti.977@gmail.com>
2024-07-08 17:09:07 +0200

    Remove latency tracking watermarks

--- flink_scripts/query2.py
+++ flink_scripts/query2.py
@@ -150,7 +150,6 @@ class CustomTimestampAssigner(TimestampAssigner):
 def query2(win):
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
-    env.get_config().set_latency_tracking_interval(500)
 
     # Setup kafka source
     source = (
