cd35c461dc25b83f988e62c2410fd8423f31bee0
onibaku <matteo.conti.977@gmail.com>
2024-07-08 23:18:27 +0200

    Rimosso bug permessi kafka init

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -1,34 +1,23 @@
 #/bin/bash
-
 #Delete old topics
 /opt/bitnami/kafka/bin/kafka-topics.sh --delete --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $FLINK_TOPIC_NAME deleted"
-
 /opt/bitnami/kafka/bin/kafka-topics.sh --delete --topic $QUERY1_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $QUERY1_TOPIC_NAME deleted"
-
 /opt/bitnami/kafka/bin/kafka-topics.sh --delete --topic $QUERY2_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $QUERY2_TOPIC_NAME deleted"
-
 /opt/bitnami/kafka/bin/kafka-topics.sh --delete --topic $QUERY3_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $QUERY3_TOPIC_NAME deleted"
-
 /opt/bitnami/kafka/bin/kafka-topics.sh --delete --topic $INGESTION_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $INGESTION_TOPIC_NAME deleted"
-
 #Create topics
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $FLINK_TOPIC_NAME --bootstrap-server kafka:9092 
 echo "topic $FLINK_TOPIC_NAME was create"
-
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY1_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY1_TOPIC_NAME --bootstrap-server kafka:9092 
 echo "topic $QUERY1_TOPIC_NAME was create"
-
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY2_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY2_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $QUERY2_TOPIC_NAME was create"
-
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY3_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $QUERY3_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $QUERY3_TOPIC_NAME was create"
-
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $INGESTION_TOPIC_NAME --bootstrap-server kafka:9092 --if-not-exists
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic $INGESTION_TOPIC_NAME --bootstrap-server kafka:9092
 echo "topic $INGESTION_TOPIC_NAME was create"
-
